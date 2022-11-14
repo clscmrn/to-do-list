c2a67ddf63c50e97aa0df0c7cfd6a6cb53ccb962
cameron <117669443+clscmrn@users.noreply.github.com>
2022-11-14 10:06:16 -0600

    Add files via upload

--- to-do list cameron and jackson/app.js
+++ to-do list cameron and jackson/app.js
@@ -99,7 +99,25 @@ $('.random_bg').css({
  }
 // Random quote algorithm acquired from Stack Overflow
  (function newQuote() {
-    var quotes = ['Fact 1', 'Fact 2', 'Fact 183'];
+    var quotes = [
+        '\“The best way to predict your future is to create it.\” - Abraham Lincoln',
+        '\“Do what you can, with what you have, where you are.\” - Theodore Roosevelt',
+        '\“Nothing succeeds like success. Get a little success, and then just get a little more.\” - Maya Angelou', 
+        '\“It has been my observation that most people get ahead during the time that others waste.\” - Henry Ford', 
+        '\“The difference between what we do and what we are capable of doing would suffice to solve most of the world\'s problems.\” - Mahatma Gandhi', 
+        '\“You cannot change your destination overnight, but you can change your direction overnight.\” - Jim Rohn', 
+        '\“Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do.\” - Steve Jobs', 
+        '\“I wouldn\’t be where I am now if I didn\’t fail … a lot. The good, the bad, it\’s all part of the success equation.\” - Mark Cuban', 
+        '\““I just love it when people say I can\'t do it; there\'s nothing that makes me feel better because all my life, people have said that I wasn\'t going to make it.\” - Ted Turner', 
+        '\“If you can\’t tolerate critics, don\’t do anything new or interesting.\” - Jeff Bezos', 
+        '\““You don\'t make progress by standing on the sidelines, whimpering, and complaining. You make progress by implementing ideas.\” - Shirley Chisholm', 
+        '\“I really don\’t think life is about the I-could-have-beens. Life is only about the I-tried-to-do. I don\’t mind the failure, but I can\’t imagine that I\’d forgive myself if I didn\’t try.\” - Nikki Giovanni', 
+        '\“If you hear a voice within you, say, \'You cannot paint,\' then by all means, paint, and that voice will be silenced.\” - Vincent Van Gogh', 
+        '\“Someday is a disease that will take your dreams to the grave with you. Pro and con lists are just as bad. If it\’s important to you and you want to do it \'eventually,\' just do it and correct course along the way.\” - Tim Ferriss', 
+        '\“The most important thing about art is to work. Nothing else matters except sitting down every day and trying.\” - Steven Pressfield', 
+        '\“Successful people do what unsuccessful people are not willing to do — even when it doesn\’t look like it makes any difference. And they do it long enough for the compounding effect to start to kick in.\” - Jeff Olson', 
+
+    ];
     var randomQuote = Math.floor(Math.random() * quotes.length);
     document.getElementById('quoteDisplay').innerHTML = quotes[randomQuote];
   })();
